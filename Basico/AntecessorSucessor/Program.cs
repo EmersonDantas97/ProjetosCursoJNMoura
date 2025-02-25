c634e863527d3aec581043223bae25a6c48708bb
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2025-02-25 08:22:50 -0300

    commit inicial

---
+++ Basico/AntecessorSucessor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System; // FrameWork.
+/*using System.Collections.Generic; // Colecoes
+//using System.Linq; // Linguagem linq.
+using System.Text; // Funcoes para textos
+using System.Threading.Tasks; // Biblioteca de processamento paralelo ou assincrono.*/
+
+// Tipagem estática ou dinamica
+
+namespace AntecessorSucessor
+{
+    internal class Program // Internal pq é visto so neste projeto.
+    {
+        static void Main(string[] args) // Main não pode mudar, pois ele vai procurar esse. É a função principal.
+        {
+            
+            Console.Write("Digite o seu numero: ");
+
+            int numero = numero = int.Parse(Console.ReadLine()); // Inclui na memoria, armazenando 4 bytes. Valor incial é de 0, é colcado para sobrescrever, porem ele pede para colocar um valor.
+            int sucessor = numero + 1; // Não houve realocacao, so sobrescreveu a memoria.
+            int antecessor = numero - 1;
+
+            // Colocar como nome da variavel, sempre o que ela faz, nao depender de comentarios.
+            // System.Console.WriteLine(numero); // Outra maneira de chamar. Caso não queira declarar o System lá em cima.
+            
+            Console.WriteLine("Antecessor: " + numero.ToString()); // O que colocamos dentro dos parenteses, tem o nome de parametro.
+            Console.WriteLine($"Sucessor: {sucessor}\nAntecessor: {antecessor}"); // Realizando interpolacao. O ideal é colocar separado, não utilizar \n
+            Console.WriteLine("Antecessor: {0}, Sucessor: {1}.", antecessor, sucessor);
+            Console.Beep(); // C# Faz a diferenciacao de letras minusculas e maiusculas.
+
+            // Ex: WriteLine Cada palavra e maiuscula, isso e convencao da Microsoft.
+            Console.ReadLine(); //Quebra de linha nao ocupa espaco, depois que compila o programa tira os espacos e os comentarios.
+        }
+    }
+}
---
+++ Basico/Calculadora/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------ CALCULADORA");
+            Console.WriteLine();
+
+            // Obter dois números do usuário
+            Console.Write("Digite o numero 1: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o numero 2: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("------------ CALCULOS GERADOS");
+
+            //Calcular e apresentar. Operações: Soma, subração, multiplicação, divisão, resto da divisão
+            Console.WriteLine();
+
+            Console.WriteLine($"SOMA entre {numero1} e {numero2}: {numero1 + numero2}.");
+            Console.WriteLine($"SUBTRACAO entre {numero1} e {numero2}: {numero1 - numero2}.");
+            Console.WriteLine($"MULTIPLICACAO entre {numero1} e {numero2}: {numero1 * numero2}.");
+            Console.WriteLine($"DIVISÃO entre {numero1} e {numero2}: {(double) numero1 / numero2:0.00}."); // Realizando Cast, que significa conversao. Realizando formatacao também. .
+            Console.WriteLine($"O RESTO da divisão entre {numero1} e {numero2}: {numero1 % numero2}.");
+
+            Console.WriteLine();
+            Console.WriteLine("------------ FINAL");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+// Estudar sobre breakpoint e modo de debug ou depuracao.
