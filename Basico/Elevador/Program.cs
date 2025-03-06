491d0fdc1697e770515c503e04831242c50108c4
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2025-03-06 12:05:40 -0300

    Projeto elevador - utilizacao try parse

---
+++ Basico/Elevador/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+/*
+Faça um programa que mostre se um elevador pode entrar em funcionamento ou não.
+
+Primeiro, o programa deve solicitar o peso máximo, em Kg, suportado pelo elevador e o número de pessoas que desejam utilizá-lo. 
+
+Depois, solicitar o peso de cada pessoa e, no final, exibir se o elevador poderá entrar em funcionamento ou não. 
+
+Caso a soma dos pesos das pessoas for maior que o peso máximo suportado pelo elevador, ele não poderá entrar em funcionamento.
+ */
+
+namespace Elevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double pesoSuportadoPeloElevador;
+            int numeroPessoas;
+            double pesoDasPessoas;
+
+            int i;
+            bool deuCerto;
+
+            Console.WriteLine("\n--------- ELEVADOR");
+
+            Console.Write("\nDigite o peso MÁXIMO SUPORTADO pelo elevador em Kg: ");
+            pesoSuportadoPeloElevador = double.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.Write("\nNúmero de pessoas que desejam utilizá-lo: ");
+                deuCerto = int.TryParse(Console.ReadLine(), out numeroPessoas);
+            } while (!deuCerto || numeroPessoas < 0);
+
+            //numeroPessoas = int.Parse(Console.ReadLine()); 
+            //Não ve se a pessoa está digitando valores errados. Ex: letras e etc. 
+
+            i = 1;
+            pesoDasPessoas = 0;
+
+            while (i <= numeroPessoas)
+            {
+                Console.Write($"\nDigite o peso da pessoa {i}: ");
+                pesoDasPessoas += double.Parse(Console.ReadLine());
+                i++;
+            }
+
+            if (pesoDasPessoas > pesoSuportadoPeloElevador)
+                Console.WriteLine("\n\tO elevador não pode operar! PESO MÁXIMO EXCEDIDO!");
+            else
+                Console.WriteLine("\n\tO elevador pode operar!");
+
+            Console.Write("\nPressione ENTER para finalizar o programa!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basico/Escopo/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+/*
+Treinamento para entendimento de criação de variáveis com base no escopo que foi criada.
+*/
+
+namespace Escopo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region "Teste de mesa 1"
+            //int x = 0;
+            //int j = 50;
+
+            //for (int i = 0; i < 2; i++)
+            //    x += 1;
+
+            //if (x < 6)
+            //{
+            //    int y = 10;
+            //    x = y + 5;
+
+            //    if(y ==10)
+            //    {
+            //        int w = 90;
+            //        x += w;
+            //    }
+            //}
+            //else
+            //{
+            //    j = 80;
+            //}
+
+            //Console.WriteLine($"X = {x}");
+            //Console.WriteLine($"J = {j}");
+            //Console.ReadLine();
+
+            //else
+            //{
+            //    int j = 10;
+            //    // int w = 0; Não pode ser declarado novamente aqui, pois já foi declarado no escopo anterior.
+            //    w = 10; // Desta maneira pode, pois eu estou apenas alterando o valor de uma variavel que ja existe no escopo superior.
+            //}
+
+            //x = x + j; // J esta somente no escopo do if.
+            #endregion
+
+            #region "Teste de mesa 2"
+
+            int i = 0, j = 0, x = 1; 
+
+            for (i = 0; i < 2;) 
+            {
+                for (j = 2; j > -1; j--) 
+                {
+                    x += i + j; //4, 5
+                }
+                i++;
+                do
+                {
+                    x += j;
+                    j++;
+                } while (j < 2);
+            }
+            while (x < 20)
+            {
+                x += 2;
+                if (x % 2 == 0)
+                {
+                    x--;
+                }
+            }
+
+            Console.WriteLine(x);
+            Console.WriteLine(j);
+            Console.WriteLine(i);
+            #endregion
+        }
+    }
+}
