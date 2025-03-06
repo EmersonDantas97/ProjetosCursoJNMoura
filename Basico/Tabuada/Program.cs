aa3046f2a1d03178241b6bf2598ce95c63bbcd76
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2025-03-06 09:14:31 -0300

    Finalizacao projeto tabuada - aprendizado laços de repetição

--- Basico/Tabuada/Program.cs
+++ Basico/Tabuada/Program.cs
@@ -16,7 +16,10 @@ namespace Tabuada
 
             Console.WriteLine();
 
-            #region "Sem estrutura de repeticao"
+            // Não tem diferença entre os laços de repetição em termos de desempenho.
+            // Professor, disse que é melhor ver como qual laço de repeticao a equipe está habituado.
+
+            #region "SOLUÇÃO 01: Sem estrutura de repeticao"
             //Console.WriteLine($"\n{numeroTabuada} X 0 = {numeroTabuada * 0}");
             //Console.WriteLine($"{numeroTabuada} X 1 = {numeroTabuada * 1}");
             //Console.WriteLine($"{numeroTabuada} X 2 = {numeroTabuada * 2}");
@@ -30,17 +33,17 @@ namespace Tabuada
             //Console.WriteLine($"{numeroTabuada} X 10 = {numeroTabuada * 10}");
             #endregion
 
-            #region "Estrutura de repetição FOR"
+            #region "SOLUÇÃO 02: Estrutura de repetição FOR"
 
-            for (int i = 0; // Vai ser repetido so a primeira vez. É "i" de iteração.
-                i <= 10; // Vai ser feito o teste todas as vezes. 
-                i++) // Vai passar todas as vezes, menos na primeira. 
-            { 
-                Console.WriteLine($"{numeroTabuada} X {i} = {numeroTabuada * i}");
-            }
+            //for (int i = 0; // Vai ser repetido so a primeira vez. É "i" de iteração.
+            //    i <= 10; // Vai ser feito o teste todas as vezes. 
+            //    i++) // Vai passar todas as vezes, menos na primeira. 
+            //{ 
+            //    Console.WriteLine($"{numeroTabuada} X {i} = {numeroTabuada * i}");
+            //}
             #endregion
 
-            #region "Estrutura de repetição WHILE"
+            #region "SOLUÇÃO 03: Estrutura de repetição WHILE"
             //int contador1 = 0;
             //while (contador1 <= 10)
             //{
@@ -49,14 +52,13 @@ namespace Tabuada
             //}
             #endregion
 
-            #region "Estrutura de repetição DO WHILE"
-            //int contador1 = 0;
-
-            //do while (contador1 < 11)
-            //{
-            //        Console.WriteLine($"{numeroTabuada} X {contador1} = {numeroTabuada * contador1}");
-            //        contador1++;
-            //}
+            #region "SOLUÇÃO 04: Estrutura de repetição DO WHILE"
+            int contador2 = 0;
+            do // Executa pelo menos 1x. Teste lógico no final.
+            {
+               Console.WriteLine($"{numeroTabuada} X {contador2} = {numeroTabuada * contador2}");
+                contador2++;
+            } while (contador2 < 11);
             #endregion
 
             Console.Write("\nPressione ENTER para finalizar o programa!");
