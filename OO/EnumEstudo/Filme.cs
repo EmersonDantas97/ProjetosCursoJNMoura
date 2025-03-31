21250957ffdecd84bf071482f651660d1faa5333
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2025-03-31 18:35:48 -0300

    Adicionado codigo no teste de enum

---
+++ OO/EnumEstudo/Filme.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumEstudo
+{
+    public enum Genero {Comedia, Terror, Acao, Suspense, FiccaoCientifica }
+
+    internal class Filme
+    {
+        public string Nome;
+        public Genero Genero;
+
+        public Filme()
+        {
+        }
+        public Filme(string Nome, Genero Genero) 
+        {
+            this.Nome = Nome;
+            this.Genero = Genero;
+        }
+    }
+}
--- OO/EnumEstudo/Program.cs
+++ OO/EnumEstudo/Program.cs
@@ -16,12 +16,32 @@ namespace EnumEstudo
 
             int pedido = numeroAleatorio.Next(1,4);
 
-            Console.WriteLine($"O status do pedido é: {Enum.GetName(typeof(statusPedido), pedido)}");
+            Console.WriteLine($"O status do pedido é: {Enum.GetName(typeof(statusPedido), pedido)}"); // Acessando nome do enum
 
             Console.Write($"Os valores do Enum são: ");
 
-            foreach (var item in Enum.GetValues(typeof(statusPedido)))
+            foreach (var item in Enum.GetValues(typeof(statusPedido))) // Acessando todos os nomes do enum
                 Console.Write($"{item} ");
+
+
+            Filme filmeParaFamilia = new Filme();
+            filmeParaFamilia.Nome = "Sharknado";
+            filmeParaFamilia.Genero = Genero.Comedia; // Utilizando enum em uma classe
+
+            Console.WriteLine($"\n\nO filme <{filmeParaFamilia.Nome}> tem como genero <{filmeParaFamilia.Genero}>, e o id do gênero é <{(int)filmeParaFamilia.Genero}>.\n");
+            Console.WriteLine();
+
+            Filme filmeCelula = new Filme();
+            filmeCelula.Nome = "Até o último homem"; // Utilizando enum em uma classe
+            filmeCelula.Genero = Genero.Acao;
+
+            Console.WriteLine();
+            Console.WriteLine($"O filme <{filmeCelula.Nome}> tem como genero <{filmeCelula.Genero}>, e o id do gênero é <{(int)filmeCelula.Genero}>.");
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+
 
         }
     }
