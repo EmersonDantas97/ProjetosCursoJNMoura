fc437c652d3acd8327f0c4b3aae326e5f28d7064
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2025-04-23 23:03:18 -0300

    Adicionado classe empresa

---
+++ Web/web-api/Models/Empresa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace web_api.Models
+{
+    public class Empresa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cnpj { get; set; }
+        public DateTime DataAbertura { get; set; }
+
+        public Empresa()
+        {
+        }
+    }
+}
