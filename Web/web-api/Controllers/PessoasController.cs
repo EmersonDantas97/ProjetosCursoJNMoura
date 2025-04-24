25effc9dd0fb5a05e18862ea00fed21c59e21978
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2025-04-24 12:11:12 -0300

    Criado projeto da APi para Crud de lista de brinquedos

--- Web/web-api/Controllers/PessoasController.cs
+++ Web/web-api/Controllers/PessoasController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using web_api.Models;
 
 namespace web_api.Controllers
 {
@@ -11,17 +12,19 @@ namespace web_api.Controllers
     {
         private static List<Models.Pessoa> pessoas = new List<Models.Pessoa>();
 
-        public PessoasController()
+        public PessoasController() 
         {
         }
 
         // GET: api/Pessoas
+        [HttpGet]
         public IEnumerable<Models.Pessoa> Get()
         {
             return pessoas;
         }
 
         // GET: api/Pessoas/5
+        [HttpGet]
         public Models.Pessoa Get(int id)
         {
             foreach (var item in pessoas)
@@ -31,17 +34,29 @@ namespace web_api.Controllers
                     return item;
                 }
             }
-
             return null;
         }
 
         // POST: api/Pessoas
+        [HttpPost]
         public void Post([FromBody] Models.Pessoa pessoa)
         {
             pessoas.Add(pessoa);
         }
 
+        // POST: api/Pessoas
+        [HttpPost]
+        [Route("api/Pessoas/Lote")]
+        public void Post([FromBody] List<Models.Pessoa> pessoasLista)
+        {
+            foreach(var item in pessoasLista)
+            {
+                pessoas.Add(item);
+            }
+        }
+
         // PUT: api/Pessoas/5
+        [HttpPut]
         public void Put(int id, [FromBody] Models.Pessoa pessoa)
         {
             foreach (var item in pessoas)
@@ -56,6 +71,7 @@ namespace web_api.Controllers
         }
 
         // DELETE: api/Pessoas/5
+        [HttpDelete]
         public void Delete(int id)
         {
             foreach (var item in pessoas)
---
+++ Web/web-api/Models/Brinquedo.cs
@@ -0,0 +1,16 @@
+﻿namespace web_api.Models
+{
+    public class Brinquedo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+        public int IdadeMinima { get; set; }
+        public int IdadeMaxima { get; set; }
+
+        public Brinquedo()
+        {
+        }
+
+    }
+}
